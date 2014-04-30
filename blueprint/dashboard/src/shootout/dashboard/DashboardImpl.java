1149e65c089072521ddcb7f15acb84d5f9166d82
Sander Mak <sander.mak@luminis.eu>
2014-04-30 23:37:35 +0200

    'Feature parity' with Declarative Services example

--- blueprint/dashboard/src/shootout/dashboard/DashboardImpl.java
+++ blueprint/dashboard/src/shootout/dashboard/DashboardImpl.java
@@ -1,12 +1,12 @@
 package shootout.dashboard;
 
 import java.util.Timer;
+import java.util.TimerTask;
 
 import shootout.dashboard.api.Dashboard;
 import shootout.sensor.api.Sensor;
 
 public class DashboardImpl implements Dashboard {
-
 
 	private Sensor sensor;
 	private Timer timer;
@@ -20,22 +20,25 @@ public class DashboardImpl implements Dashboard {
 		System.out.println("Reading: " + sensor.getReading());
 	}
 
-//	protected void activate(ComponentContext context) {
-//		System.out.println("DashboardImpl activated");
-//		timer = new Timer();
-//		TimerTask task = new TimerTask() {
-//			public void run() {
-//				showDashboard();
-//			}
-//		};
-//		timer.scheduleAtFixedRate(task, 0, 10000);
-//
-//	}
-//
-//	protected void deactivate(ComponentContext context) {
-//		timer.cancel();
-//		timer = null;
-//	}
+	// No special name, referenced with init-method property. Must be public.
+	public void myActivate() {
+		System.out.println("DashboardImpl activated");
+		timer = new Timer();
+		TimerTask task = new TimerTask() {
+			public void run() {
+				showDashboard();
+			}
+		};
+		timer.scheduleAtFixedRate(task, 0, 10000);
+
+	}
+
+	// No special name, referenced with destroy-method property. Must be public.
+	public void myDeactivate() { 
+		System.out.println("DashboardImpl deactivated");
+		timer.cancel();
+		timer = null;
+	}
 	
 	public synchronized void setSensor(Sensor sensor) {
 		this.sensor = sensor;
